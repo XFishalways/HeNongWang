0a60daf57431b337c3364b021940e84c45d00d5d
XFish <18901030225@163.com>
2022-07-13 18:02:08 +0800

    BuyerUserr

--- src/main/java/com/bug/henong/entity/BuyerUser.java
+++ src/main/java/com/bug/henong/entity/BuyerUser.java
@@ -4,138 +4,141 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import javax.persistence.*;
 import java.io.Serializable;
+import java.sql.Time;
+import java.sql.Timestamp;
 import java.util.Date;
 
 /**
- * ���;
- * @author : http://www.chiner.pro
+ * 买家;
+ * @author : TAN
  * @date : 2022-7-13
  */
-@ApiModel(value = "���",description = "")
+@ApiModel(value = "买家",description = "")
 @Table(name="BUYER_USER")
 public class BuyerUser implements Serializable,Cloneable{
-    /** �û�ID */
+    /** 用户ID */
     @Id
     @GeneratedValue
-    @ApiModelProperty(name = "�û�ID",notes = "")
+    @ApiModelProperty(name = "用户ID",notes = "")
     private String userId ;
-    /** �û����� */
-    @ApiModelProperty(name = "�û�����",notes = "")
+    /** 用户名称 */
+    @ApiModelProperty(name = "用户名称",notes = "")
     private String userName ;
-    /** �û��ǳ� */
-    @ApiModelProperty(name = "�û��ǳ�",notes = "")
+    /** 用户昵称 */
+    @ApiModelProperty(name = "用户昵称",notes = "")
     private String nickName ;
-    /** ����ǩ�� */
-    @ApiModelProperty(name = "����ǩ��",notes = "")
+    /** 个性签名 */
+    @ApiModelProperty(name = "个性签名",notes = "")
     private String userIntro ;
-    /** ͷ��ͼƬ */
-    @ApiModelProperty(name = "ͷ��ͼƬ",notes = "")
+    /** 头像图片 */
+    @ApiModelProperty(name = "头像图片",notes = "")
     private String avatar ;
-    /** �ֻ��� */
-    @ApiModelProperty(name = "�ֻ���",notes = "")
+    /** 手机号 */
+    @ApiModelProperty(name = "手机号",notes = "")
     private String phone ;
-    /** ���� */
-    @ApiModelProperty(name = "����",notes = "")
+    /** 密码 */
+    @ApiModelProperty(name = "密码",notes = "")
     private String userPass ;
-    /** ������ */
-    @ApiModelProperty(name = "������",notes = "")
+    /** 密码盐 */
+    @ApiModelProperty(name = "密码盐",notes = "")
     private String passSalt ;
-    /** �û�״̬ */
-    @ApiModelProperty(name = "�û�״̬",notes = "")
+    /** 用户状态 */
+    @ApiModelProperty(name = "用户状态̬",notes = "")
     private String userStatus ;
-    /** �ۼ����ѽ�� */
-    @ApiModelProperty(name = "�ۼ����ѽ��",notes = "")
+    /** 累计消费金额 */
+    @ApiModelProperty(name = "累计消费金额",notes = "")
     private Double totalCostAmt ;
-    /** ����¼ʱ�� */
-    @ApiModelProperty(name = "����¼ʱ��",notes = "")
-    private Date lastLoginTime ;
+    /** 最后登录时间 */
+    @ApiModelProperty(name = "最后登录时间",notes = "")
+    private Timestamp lastLoginTime ;
 
-    /** �����û�ID */
+
+    /** 返回用户ID */
     public String getUserId(){
         return this.userId;
     }
-    /** �����û�ID */
+    /** 设置用户ID */
     public void setUserId(String userId){
         this.userId=userId;
     }
-    /** �����û����� */
+    /** 返回用户名称 */
     public String getUserName(){
         return this.userName;
     }
-    /** �����û����� */
+    /** 设置用户名称 */
     public void setUserName(String userName){
         this.userName=userName;
     }
-    /** �����û��ǳ� */
+    /** 返回用户昵称 */
     public String getNickName(){
         return this.nickName;
     }
-    /** �����û��ǳ� */
+    /** 设置用户昵称 */
     public void setNickName(String nickName){
         this.nickName=nickName;
     }
-    /** ���ظ���ǩ�� */
+    /** 返回个性签名 */
     public String getUserIntro(){
         return this.userIntro;
     }
-    /** ���ø���ǩ�� */
+    /** 设置个性签名 */
     public void setUserIntro(String userIntro){
         this.userIntro=userIntro;
     }
-    /** ����ͷ��ͼƬ */
+    /** 返回头像图片 */
     public String getAvatar(){
         return this.avatar;
     }
-    /** ����ͷ��ͼƬ */
+    /** 设置头像图片 */
     public void setAvatar(String avatar){
         this.avatar=avatar;
     }
-    /** �����ֻ��� */
+    /** 返回手机号 */
     public String getPhone(){
         return this.phone;
     }
-    /** �����ֻ��� */
+    /** 设置手机号 */
     public void setPhone(String phone){
         this.phone=phone;
     }
-    /** �������� */
+    /** 返回密码 */
     public String getUserPass(){
         return this.userPass;
     }
-    /** �������� */
+    /** 设置密码 */
     public void setUserPass(String userPass){
         this.userPass=userPass;
     }
-    /** ���������� */
+    /** 返回密码盐 */
     public String getPassSalt(){
         return this.passSalt;
     }
-    /** ���������� */
+    /** 设置密码盐 */
     public void setPassSalt(String passSalt){
         this.passSalt=passSalt;
     }
-    /** �����û�״̬ */
+    /** 返回用户状态 */
     public String getUserStatus(){
         return this.userStatus;
     }
-    /** �����û�״̬ */
+    /** 设置用户状态 */
     public void setUserStatus(String userStatus){
         this.userStatus=userStatus;
     }
-    /** �����ۼ����ѽ�� */
+    /** 返回累计消费金额 */
     public Double getTotalCostAmt(){
         return this.totalCostAmt;
     }
-    /** �����ۼ����ѽ�� */
+    /** 设置累计消费金额 */
     public void setTotalCostAmt(Double totalCostAmt){
         this.totalCostAmt=totalCostAmt;
     }
-    /** ��������¼ʱ�� */
-    public Date getLastLoginTime(){
+    /** 返回最后登录时间 */
+    public Timestamp getLastLoginTime(){
         return this.lastLoginTime;
     }
-    /** ��������¼ʱ�� */
-    public void setLastLoginTime(Date lastLoginTime){
+    /** 设置最后登录时间 */
+    public void setLastLoginTime(Timestamp lastLoginTime){
         this.lastLoginTime=lastLoginTime;
     }
 }
