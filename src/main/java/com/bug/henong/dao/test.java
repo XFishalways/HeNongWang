27db1196f93ab119066c31a403f9aa1d9343cffe
XFish <18901030225@163.com>
2022-07-13 19:38:21 +0800

    fixed

--- src/main/java/com/bug/henong/dao/test.java
+++ src/main/java/com/bug/henong/dao/test.java
@@ -1,18 +1,19 @@
 package com.bug.henong.dao;
 
+import com.bug.henong.entity.BuyerUser;
 import com.bug.henong.entity.Farmer;
 
 import java.sql.SQLException;
 
 public class test {
     public static  void main(String args[]) throws SQLException {
-        Farmer farmer =new Farmer();
-        farmer.setFarmerId("1");
-        farmer.setFarmerName("Tan");
-        farmer.setFarmerAge(19);
-        farmer.setFarmerValue(0.0);
-        farmer.setFarmerPlace("CD");
-        FarmerDao farmerDao = new FarmerDao();
-        System.out.println(farmerDao.insert(farmer)+"row changed");
+        BuyerUserDao userDao = new BuyerUserDao();
+        BuyerUser user = new BuyerUser();
+        user.setUserId("1");
+        user.setUserPass("11");
+        user.setPassSalt("14");
+        user.setUserName("Tan");
+        int rw = userDao.insert(user);
+        System.out.println(rw+"has changed");
     }
 }
