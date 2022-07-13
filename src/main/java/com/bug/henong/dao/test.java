5add9a78eb7ae458a68b819b5ecec3d910b5e7a3
Tan <1987559684@qq.cp>
2022-07-13 18:17:52 +0800

    test

--- src/main/java/com/bug/henong/dao/test.java
+++ src/main/java/com/bug/henong/dao/test.java
@@ -1,4 +1,20 @@
 package com.bug.henong.dao;
 
+import com.bug.henong.entity.Farmer;
+
+import java.sql.SQLException;
+
 public class test {
+    public static  void main(String args[]) throws SQLException {
+        Farmer farmer =new Farmer();
+        farmer.setFarmerId("1");
+        farmer.setFarmerName("Tan");
+        farmer.setFarmerAge(19);
+        farmer.setFarmerValue(0.0);
+        farmer.setFarmerPlace("CD");
+        FarmerDao farmerDao = new FarmerDao();
+        System.out.println(farmerDao.insert(farmer)+"row changed");
+
+
+    }
 }
