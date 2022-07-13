515c53d19418a0fdbf998136bda14b0c0c2ddaba
Tan <1987559684@qq.cp>
2022-07-13 16:23:43 +0800

    BuyerUserDao

--- src/main/java/com/bug/henong/dao/BuyerUserDao.java
+++ src/main/java/com/bug/henong/dao/BuyerUserDao.java
@@ -10,8 +10,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+/**
+ * @author Tan
+ * @version 1.0.0
+ * @date 22.07.13
+ */
 public class BuyerUserDao {
-    //添加
+
+    /**添加*/
     public int insert(BuyerUser user) throws SQLException {
         Entity entity = Entity.parse(user);
 
@@ -19,18 +25,21 @@ public class BuyerUserDao {
 
         return  rw;
     }
-    //删除
+
+    /**删除*/
     public int delete(int id) throws SQLException {
         int rw=Db.use().del(
-                Entity.create("BUYER_USER").set("id",id)
+                Entity.create("BUYER_USER").set("USER_ID",id)
         );
         return rw;
     }
+
+    /**返回所有信息*/
     public List<BuyerUser> findAll() throws SQLException {
-        String sql = "SELECT * FROM BUYERUSER";
+        String sql = "SELECT * FROM BUYER_USER";
 
         List<BuyerUser> buyerUsers = new ArrayList<BuyerUser>();
-        List<Entity> entities = Db.use().findAll("BUYERUSER");
+        List<Entity> entities = Db.use().findAll("BUYER_USER");
         for(Entity e : entities){
             String buyerStr = JSONUtil.toJsonStr(e);
             BuyerUser buyerUser = BeanUtil.toBean(buyerStr,BuyerUser.class);
@@ -38,5 +47,18 @@ public class BuyerUserDao {
         }
 
         return buyerUsers;
+    }
+
+    /**通过id查找某一行数据*/
+    public BuyerUser findOneBuyer(String id) throws SQLException {
+        List<Entity> entities= Db.use().findAll(
+                Entity.create("BUYER_USER").set("USER_ID",id)
+        );
+
+        Entity e = entities.get(0);
+        String buyerStr = JSONUtil.toJsonStr(e);
+        BuyerUser buyerUser = BeanUtil.toBean(buyerStr,BuyerUser.class);
+
+        return  buyerUser;
     }
 }
