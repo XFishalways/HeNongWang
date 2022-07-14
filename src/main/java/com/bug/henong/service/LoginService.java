3bda174492daec4467789f04de6cde525fef96f6
Tan <1987559684@qq.cp>
2022-07-14 19:09:35 +0800

    fixed

---
+++ src/main/java/com/bug/henong/service/LoginService.java
@@ -0,0 +1,43 @@
+package com.bug.henong.service;
+
+import com.bug.henong.dao.BusinessUserDao;
+import com.bug.henong.dao.BuyerUserDao;
+import com.bug.henong.dao.FarmerDao;
+
+import com.bug.henong.entity.BusinessUser;
+import com.bug.henong.entity.BuyerUser;
+import com.bug.henong.entity.Farmer;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+import java.sql.SQLException;
+
+@Service("LoginService")
+public class LoginService {
+
+
+    private FarmerDao farmerDao;
+
+    private BuyerUserDao buyerUserDao;
+
+    private BusinessUserDao businessUserDao;
+
+    /**
+     * 农户登录
+     */
+    public Farmer farmerLogin(String id, String password) throws SQLException {
+        return farmerDao.login(id, password);
+    }
+
+    /**买家登录*/
+    public BuyerUser buyerLogin(String id, String password)throws SQLException{
+        return buyerUserDao.login(id, password);
+    }
+    /**卖家登录*/
+    public BusinessUser businessUserLogin(String id, String password) throws SQLException {
+        return businessUserDao.login(id, password);
+    }
+
+
+}
