d5e08c031a21dfc351ef6348d971c6acca668251
XFish <18901030225@163.com>
2022-07-14 12:10:33 +0800

    extend fixed

--- src/main/java/com/bug/henong/servlet/FindOneBuyerUserServlet.java
+++ src/main/java/com/bug/henong/servlet/FindOneBuyerUserServlet.java
@@ -2,6 +2,7 @@ package com.bug.henong.servlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -13,8 +14,8 @@ import com.bug.henong.dao.BuyerUserDao;
 import com.bug.henong.entity.BuyerUser;
 
 @WebServlet(name = "findOneBuyerUserServlet", value = "/FindOneBuyerUser")
-public class FindOneBuyerUserServlet {
-
+public class FindOneBuyerUserServlet extends HttpServlet {
+    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("application/json;charset=UTF-8");
         PrintWriter pw = resp.getWriter();
