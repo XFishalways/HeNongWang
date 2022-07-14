dece214a29ccc347a290fe8a51b4f21cbcbb9f03
XFish <18901030225@163.com>
2022-07-14 10:36:24 +0800

    test.html fixed

--- src/main/java/com/bug/henong/servlet/getBuyerUsers.java
+++ src/main/java/com/bug/henong/servlet/getBuyerUsers.java
@@ -18,10 +18,11 @@ import com.bug.henong.entity.BuyerUser;
 public class getBuyerUsers extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("application/json");
+        resp.setContentType("application/json;charset=UTF-8");
         PrintWriter pw = resp.getWriter();
         BuyerUserDao buyerUserDao = new BuyerUserDao();
         String id = req.getParameter("USER_ID");
+        System.out.println(id);
         String json = null;
         try {
             BuyerUser user = buyerUserDao.findOneBuyer(id);
@@ -34,6 +35,7 @@ public class getBuyerUsers extends HttpServlet {
         } catch (SQLException e) {
             json = "{\"log\":\"Error connection to database\"}";
         }
+        System.out.println(json);
         pw.print(json);
     }
 }
