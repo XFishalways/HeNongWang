13ec16a80969e81e2e23ba34725e1ff7697750fe
XFish <18901030225@163.com>
2022-07-15 10:38:28 +0800

    PoisonDogSb

--- src/main/java/com/bug/henong/controller/LoginController.java
+++ src/main/java/com/bug/henong/controller/LoginController.java
@@ -4,6 +4,7 @@ import com.bug.henong.entity.BusinessUser;
 import com.bug.henong.entity.BuyerUser;
 import com.bug.henong.entity.Farmer;
 import com.bug.henong.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
