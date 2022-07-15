83f3003692474bcb9166b924c42a79aada1a6f11
XFish <18901030225@163.com>
2022-07-15 14:09:24 +0800

    LoginController.java fixed

--- src/main/java/com/bug/henong/controller/LoginController.java
+++ src/main/java/com/bug/henong/controller/LoginController.java
@@ -7,7 +7,6 @@ import com.bug.henong.service.LoginService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -23,8 +22,7 @@ public class LoginController {
     private LoginService loginService = new LoginService();
     @GetMapping(value = "/login")
     public String login() {
-        //填入返回地址
-        System.out.println("GetSuccess");
+
         return "/login";
     }
 
@@ -34,36 +32,38 @@ public class LoginController {
                         @RequestParam("password") String password,
                         @RequestParam("type") String type,
                         HttpSession session) throws SQLException {
-        System.out.println("In");
+
         if (userId==null || password==null) {
             session.setAttribute("errorMsg", "用户名或密码不能为空");
             return null;
         }
+
         switch (type){
+
             case "farmer":
-                System.out.println("2");
                 Farmer farmer = loginService.farmerLogin(userId,password);
                 if(farmer!=null){
                     System.out.println("3");
                     session.setAttribute("userId", farmer.getFarmerName());
                     session.setAttribute("loginUserId", farmer.getFarmerId());
-                    //session过期时间设置为7200秒 即两小时
+
                     return getStringObjectMap(session,type);
                 }
+
             case "buyer":
                 BuyerUser buyerUser = loginService.buyerLogin(userId,password);
                 if(buyerUser!=null){
                     session.setAttribute("loginUser", buyerUser.getUserName());
                     session.setAttribute("loginUserId", buyerUser.getUserId());
-                    //session过期时间设置为7200秒 即两小时
+
                     return getStringObjectMap(session,type);
                 }
+
             case "business":
                 BusinessUser businessUser = loginService.businessUserLogin(userId,password);
                 if(businessUser!=null){
                     session.setAttribute("loginUser", businessUser.getUserName());
                     session.setAttribute("loginUserId", businessUser.getUserId());
-                    //session过期时间设置为7200秒 即两小时
 
                     return getStringObjectMap(session,type);
                 }
@@ -74,13 +74,16 @@ public class LoginController {
     }
 
     private Map<String, Object> getStringObjectMap(HttpSession session,String type) {
+
         session.setMaxInactiveInterval(60 * 60 * 2);
+        //session过期时间设置为7200秒 即两小时
 
         Map<String,Object> map = new HashMap<>();
         map.put("message","ok");
         map.put("code",0);
         map.put("success","success");
-        map.put("type",type)
+        map.put("type",type);
+
         return map;
     }
 }
