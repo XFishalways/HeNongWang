5f7b8dbe9829b948eb725909cd275ca860f70a0e
LideRito <lide616@qq.com>
2022-07-15 11:16:54 +0800

    update myCookie.js to use cookie

---
+++ src/main/webapp/js/myCookie.js
@@ -0,0 +1,30 @@
+function createCookie(cName,cValue){
+    var tmp = "";
+    document.cookie = tmp + cName + "=" + cValue;
+}
+
+function getCookie(cName){
+    var name = cName + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name)==0) { return c.substring(name.length,c.length); }
+    }
+    return "";
+}
+
+function checkCookie(cName){
+    var user = getCookie(cName);
+    alert(1);
+    if(user != '') {
+        alert("OK");
+    }else{
+        alert("NOOOOO!");
+    }
+}
+
+function deleteCookie(cName){
+    var date = new Date();
+    date.setTime(date.getTime() - 100);
+    document.cookie = cName + "=a; expires=" + date.toGMTString(); 
+}
