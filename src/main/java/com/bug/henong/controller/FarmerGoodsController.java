2b6457ef3fab67ecb30976366e20b762dc361b99
XFish <18901030225@163.com>
2022-07-15 16:37:27 +0800

    Project fixed

---
+++ src/main/java/com/bug/henong/controller/FarmerGoodsController.java
@@ -0,0 +1,20 @@
+package com.bug.henong.controller;
+
+import com.bug.henong.utils.MapFactory;
+import com.bug.henong.entity.BusinessUser;
+import com.bug.henong.entity.BuyerUser;
+import com.bug.henong.entity.Farmer;
+import com.bug.henong.service.LoginService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.sql.SQLException;
+import java.util.Map;
+
+/**
+ *
+ */
+public class FarmerGoodsController {
+
+}
