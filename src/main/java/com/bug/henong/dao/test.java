09fa2b3885de10cab9f3a4fddbf0cc5254256cb4
XFish <18901030225@163.com>
2022-07-16 19:41:14 +0800

    fixed

--- src/main/java/com/bug/henong/dao/test.java
+++ src/main/java/com/bug/henong/dao/test.java
@@ -1,19 +1,20 @@
 package com.bug.henong.dao;
 
+import cn.hutool.db.Db;
+import cn.hutool.db.Entity;
+import com.alibaba.fastjson.JSON;
 import com.bug.henong.entity.BuyerUser;
 import com.bug.henong.entity.Farmer;
+import com.bug.henong.entity.Goods;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class test {
     public static  void main(String args[]) throws SQLException {
-        BuyerUserDao buyerUserDao = new BuyerUserDao();
-        for(int i =0; i<50;i++){
-            BuyerUser buyerUser =new BuyerUser();
-            buyerUser.setUserId(Integer.toString(i));
-            buyerUser.setUserName(Integer.toString(i));
-            buyerUserDao.insert(buyerUser);
-        }
-
+       GoodsDao goodsDao = new GoodsDao();
+        List<Entity> entities = Db.use().query("SELECT * FROM GOODS WHERE FARMER_ID LIKE ?", "%"+1+"%");
+       String json = JSON.toJSONString(entities);
+       System.out.println(json);
     }
 }
