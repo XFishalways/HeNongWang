eb399e69ce6744ecee5952a7a931676b82bfa758
GComRui <1505956111@qq.com>
2022-07-18 01:12:03 +0800

    SM3 encrypt version 3.

--- src/main/java/com/bug/henong/utils/EncryptUtil.java
+++ src/main/java/com/bug/henong/utils/EncryptUtil.java
@@ -1,13 +1,14 @@
+package com.bug.henong.utils;
 import cn.hutool.crypto.digest.DigestUtil;
 import cn.hutool.crypto.digest.Digester;
 
 public class EncryptUtil {
 
     //构造
-    private Digester digester = DigestUtil.digester("sm3");
+    private static Digester digester = DigestUtil.digester("sm3");
 
     //返回Hash值
-    public String getDigestHex(String password, String salt) {
+    public static String getDigestHex(String password, String salt) {
 
         String digestHex_1 = digester.digestHex(password);
 
